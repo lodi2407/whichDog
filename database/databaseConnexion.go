e95360edc518a721aa5b49c11e87c406dfb882e4
lodi2407 <lodi2407@laposte.net>
2023-08-27 16:24:46 +0200

    fix: from mysql to postgresSql + added env file

--- database/databaseConnexion.go
+++ database/databaseConnexion.go
@@ -1,13 +1,23 @@
 package database
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+
 	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func DatabaseConnexion() *sqlx.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	  }
+
+
 	// Open a connection to the database
-	db, err := sqlx.Connect("mysql", "root:@tcp(localhost:3306)/dogs")
+	db, err := sqlx.Connect("postgres", "postgres://" + os.Getenv("DB_USER")  + ":" + os.Getenv("DB_PASS")  + "@" + os.Getenv("DB_HOST") + "/" + os.Getenv("DB_NAME")  + "?sslmode=disable")
 	if err != nil {
 		panic(err.Error())
 	}
--- go.mod
+++ go.mod
@@ -7,3 +7,8 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.5
 )
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
