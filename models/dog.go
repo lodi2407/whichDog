aa4be7da8da7e87c58f9afc2ceb10d6b580f55eb
lodi2407 <lodi2407@laposte.net>
2024-06-15 09:27:34 +0200

    fix(model): rename db field after changes in db

--- models/dog.go
+++ models/dog.go
@@ -19,43 +19,43 @@ type Dog struct {
 }
 
 type DogHeight struct {
-	MinHeight *float32 `json:"minHeight,omitempty" db:"min_height"`
-	MaxHeight *float32 `json:"maxHeight,omitempty" db:"max_height"`
+	MinHeight *float32 `json:"minHeight,omitempty" db:"minheight"`
+	MaxHeight *float32 `json:"maxHeight,omitempty" db:"maxheight"`
 }
 
 type DogWeight struct {
-	MinWeight *float32 `json:"minWeight,omitempty"  db:"min_weight"`
-	MaxWeight *float32 `json:"maxWeight,omitempty" db:"max_weight"`
+	MinWeight *float32 `json:"minWeight,omitempty"  db:"minweight"`
+	MaxWeight *float32 `json:"maxWeight,omitempty" db:"maxweight"`
 }
 
 type DogExpectancy struct {
-	MinExpectancy *int `json:"minExpectancy,omitempty" db:"min_expectancy"`
-	MaxExpectancy *int `json:"maxExpectancy,omitempty" db:"max_expectancy"`
+	MinExpectancy *int `json:"minExpectancy,omitempty" db:"minexpectancy"`
+	MaxExpectancy *int `json:"maxExpectancy,omitempty" db:"maxexpectancy"`
 }
 
 type DogGrooming struct {
-	GroomingFrequencyValue    *float32 `json:"groomingFrequencyValue,omitempty" db:"grooming_frequency_value"`
-	GroomingFrequencyCategory *string  `json:"groomingFrequencyCategory,omitempty" db:"grooming_frequency_category"`
+	GroomingFrequencyValue    *float32 `json:"groomingFrequencyValue,omitempty" db:"groomingfrequencyvalue"`
+	GroomingFrequencyCategory *string  `json:"groomingFrequencyCategory,omitempty" db:"groomingfrequencycategory"`
 }
 
 type DogShedding struct {
-	SheddingValue    *float32 `json:"sheddingValue,omitempty" db:"shedding_value"`
-	SheddingCategory *string  `json:"sheddingCategory,omitempty"  db:"shedding_category"`
+	SheddingValue    *float32 `json:"sheddingValue,omitempty" db:"sheddingvalue"`
+	SheddingCategory *string  `json:"sheddingCategory,omitempty"  db:"sheddingcategory"`
 }
 
 type DogEnergy struct {
-	EnergyLevelValue    *float32 `json:"energyLevelValue,omitempty"  db:"energy_level_value"`
-	EnergyLevelCategory *string  `json:"energyLevelCategory,omitempty"  db:"energy_level_category"`
+	EnergyLevelValue    *float32 `json:"energyLevelValue,omitempty"  db:"energylevelvalue"`
+	EnergyLevelCategory *string  `json:"energyLevelCategory,omitempty"  db:"energylevelcategory"`
 }
 
 type DogTrainability struct {
-	TrainabilityValue    *float32 `json:"trainabilityValue,omitempty"  db:"trainability_value"`
-	TrainabilityCategory *string  `json:"trainabilityCategory,omitempty"  db:"trainability_category"`
+	TrainabilityValue    *float32 `json:"trainabilityValue,omitempty"  db:"trainabilityvalue"`
+	TrainabilityCategory *string  `json:"trainabilityCategory,omitempty"  db:"trainabilitycategory"`
 }
 
 type DogDemeanor struct {
-	DemeanorValue    *float32 `json:"demeanorValue,omitempty"  db:"demeanor_value"`
-	DemeanorCategory *string  `json:"demeanorCategory,omitempty"  db:"demeanor_category"`
+	DemeanorValue    *float32 `json:"demeanorValue,omitempty"  db:"demeanorvalue"`
+	DemeanorCategory *string  `json:"demeanorCategory,omitempty"  db:"demeanorcategory"`
 }
 
 // for test
